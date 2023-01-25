e2eaefa6988f35b5285446cd73386be0e7f4cc71
fallnomega <fallnomega@gmail.com>
2023-01-25 11:43:50 -0800

    black jack updates

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ def init_cards():
 
 
 def deal_card(hand):
+    """Returns a random card from the deck."""
+
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     hand.append(random.choice(cards))
     return hand
 
-def score (hand,user):
+def calculate_score(hand,user):
     score = 0
     for x in hand:
         score+=x
@@ -61,8 +63,8 @@ if play == 'y':
     player_cards = init_cards()
     print(f"Your cards: {player_cards} and Computer's first card: {player_cards[0]}")
     computer_cards = init_cards()
-    player_score = score(player_cards,'Player')
-    computer_score = score(computer_cards,"Dealer")
+    player_score = calculate_score(player_cards,'Player')
+    computer_score = calculate_score(computer_cards,"Dealer")
     keep_playing = blackjack(player_cards,'Player')
     keep_playing = blackjack(computer_cards,'Dealer')
     while keep_playing == True:
@@ -74,17 +76,17 @@ if play == 'y':
             exit()
         elif hit =='y':
             player_cards = deal_card(player_cards)
-            player_score = score(player_cards, 'Player')
+            player_score = calculate_score(player_cards, 'Player')
             keep_playing = blackjack(player_cards, 'Player')
             if computer_score < 17:
                 computer_cards = deal_card(computer_cards)
-                computer_score = score(computer_cards,'Computer')
+                computer_score = calculate_score(computer_cards,'Computer')
                 keep_playing = blackjack(computer_cards,'Computer')
 
         elif hit =='n':
             if computer_score < 17:
                 computer_cards = deal_card(computer_cards)
-                computer_score = score(computer_cards,'Computer')
+                computer_score = calculate_score(computer_cards,'Computer')
                 keep_playing = blackjack(computer_cards,'Computer')
             else:
                 if computer_score > player_score:
@@ -104,43 +106,7 @@ else:
 
 
 
-############### Our Blackjack House Rules #####################
 
-## The deck is unlimited in size.
-## There are no jokers.
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
-
-##################### Hints #####################
-
-#Hint 1: Go to this website and try out the Blackjack game:
-#   https://games.washingtonpost.com/games/blackjack/
-#Then try out the completed Blackjack project here:
-#   http://blackjack-final.appbrewery.repl.run
-
-#Hint 2: Read this breakdown of program requirements:
-#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
-#Then try to create your own flowchart for the program.
-
-#Hint 3: Download and read this flow chart I've created:
-#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
-
-#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
-#11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-#user_cards = []
-#computer_cards = []
-
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input
-#and returns the score.
-#Look up the sum() function to help you do this.
 
 #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
 
